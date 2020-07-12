d6b35ea80e31dc068b5e110731ba4f5a128f57cb
Ira Gnativ <irina.gnativ@gmail.com>
2020-07-12 16:40:46 +0200

    Added data display from local files using File object

--- src/App.js
+++ src/App.js
@@ -1,10 +1,57 @@
-import React from "react";
-import { parseData } from "./parsing";
+import React, { useRef, useState, useEffect } from "react";
+import Papa from "papaparse";
 
 const App = () => {
-  main();
+  const fileInput = useRef();
+  const [data, setData] = useState([]);
+  const [dataKeys, setDataKeys] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    Papa.parse(fileInput.current.files[0], {
+      complete: function (results) {
+        setData(results.data);
+        setDataKeys(Object.keys(results.data[0]));
+      },
+      header: true,
+    });
+  };
+  console.log(data);
+  let myMap = new Map();
+  for (let i = 0; i < data.length; i++) {
+    myMap.set(data[i].email, data[i]);
+  }
+  console.log(myMap.get("null-walter@echocat.org"));
+
   return (
-    <div>{/* to do: add parsed data to DOM, with displayData function*/}</div>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="file" id="input" ref={fileInput}></input>
+        <br></br>
+        <button type="submit">Submit</button>
+        <div>
+          <table>
+            <thead>
+              <tr>
+                {dataKeys.map((key) => (
+                  <td>{key}</td>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((item) => (
+                <tr>
+                  {Object.values(item).map((element) => (
+                    <td>{element}</td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </form>
+    </div>
   );
 };
 
--- src/parsing.js
+++ src/parsing.js
@@ -1,45 +1,81 @@
 // import React from "react";
 import Papa from "papaparse";
 
-const file = "../data/authors.csv";
+// const file = "../data/authors.csv";
 
-export const main = () {
-  files = ["../authors.csv", "../books.csv", "../magazines.csv"]
-  objs = []
-  for (let i = 0; i < files.length; i++) {
-    obj = createObjectFromFile(file[i])
-    objs.push(obj)
-  }
-  displayData(objs)
-};
-
-export const loadCSVfile = (file) => {
-  let data = Papa.parse(file, {
-    step: function (results) {
-      console.log("steps");
-    },
+export const main = () => {
+  Papa.parse(fileInput.files[0], {
     complete: function (results) {
-      console.log("Finished:", results.data);
-      displayData(results);
+      console.log(results);
     },
-    error: function (results) {
-      console.log("Error:", results);
-    },
-    download: true,
   });
-  console.log("Data:");
-  console.log(data);
 };
 
-export const createObjectsfromCSVData = (data, obj) => {
-  // to do: creates object from CSV using headers to define field names
-};
+// export const loadCSVfile = (file) => {
+//   let data = Papa.parse(file, {
+//     step: function (results) {
+//       console.log("steps");
+//     },
+//     complete: function (results) {
+//       console.log("Finished:", results.data);
+//       displayData(results);
+//     },
+//     error: function (results) {
+//       console.log("Error:", results);
+//     },
+//     download: true,
+//   });
+//   console.log("Data:");
+//   console.log(data);
+// };
 
-export const displayData = (obsj) => {
-  // to do: create DOM object with the data received
-};
+// export const main = () => {
+//   const files = ["../authors.csv", "../books.csv", "../magazines.csv"];
+//   const objs = [];
+//   for (let i = 0; i < files.length; i++) {
+//     let obj = createObjectFromFile(file[i]);
+//     objs.push(obj);
+//   }
+//   displayData(objs);
+// };
 
-export const createObjectFromFile = (file) => {
-  //loadCSVfile(file)
-  //createObjectsfromCSVData(data, obj)
-}
+// export const loadCSVfile = (file) => {
+//   let data = Papa.parse(file, {
+//     step: function (results) {
+//       console.log("steps");
+//     },
+//     complete: function (results) {
+//       console.log("Finished:", results.data);
+//       displayData(results);
+//     },
+//     error: function (results) {
+//       console.log("Error:", results);
+//     },
+//     download: true,
+//   });
+//   console.log("Data:");
+//   console.log(data);
+// };
+
+// export const createObjectsfromCSVData = (data, obj) => {
+//   // to do: creates object from CSV using headers to define field names
+// };
+
+// export const displayData = (obsj) => {
+//   // to do: create DOM object with the data received
+// };
+
+// export const createObjectFromFile = (file) => {
+//   //loadCSVfile(file)
+//   //createObjectsfromCSVData(data, obj)
+// };
+
+// export const main = () => {
+//   const files = ["../authors.csv", "../books.csv", "../magazines.csv"];
+//   const objs = [];
+//   for (let i = 0; i < files.length; i++) {
+//     let obj = createObjectFromFile(file[i]);
+//     objs.push(obj);
+//   }
+//   displayData(objs);
+// };
